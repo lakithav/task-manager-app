62a7f044c5f53d4d697802b773f1c765483fd3a3
lakitha21 <lakithaviraj21@gmail.com>
2025-10-13 15:41:20 +0530

    ove AutoConfigureMockMvc to correct test file

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc // This annotation belongs HERE
 class DemoApplicationTests {
 
 	@Test
